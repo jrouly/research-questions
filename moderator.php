99e9b95ac526c57416f79029cec550166e255e18
Jean Michel Rouly <jrouly@gmu.edu>
2013-10-11 21:58:35 -0400

    Set up framework for moderator page.

--- moderator.php
+++ moderator.php
@@ -16,16 +16,51 @@
   ########################################## NOT A MODERATOR
   } else { 
   ########################################## IS A MODERATOR
+  require "moderator_lib.php";
 ?>
 
 <h1>Administrator Page</h1>
 
 <p>You may use the interface below to moderate the discussion.</p>
 
-<form name="modForm" method="post" action="moderator_processor.php">
-<label for="reg-username">Username:</label>
-<input type="text" name="reg-username" id="reg-username" />
+<h3>Register New Users</h3>
+<form name="register-user" method="post" action="">
+<input type="text" name="reg-username" id="reg-username" value="username" />
+<input type="text" name="reg-name" id="reg-name" value="real name" />
+<select name="reg-level" id="reg-level">
+  <option value="moderator">Professor</option>
+  <option value="moderator">GTA</option>
+  <option value="moderator">PRM</option>
+  <option value="student" selected="selected">Student</option>
+</select>
+<a href="#">add row</a>
+<br /><br />
+<input type="submit" value="Register" name="reg-user-submit" id="reg-user-submit" />
+<br /><br />
+<?php process_register_user(); ?>
+&nbsp;
 </form>
+
+<h3>Modify an Existing User</h3>
+<form name="modify-user" method="post" action="">
+list users to modify, selecting one brings up modification dialog
+<input type="submit" value="Register" name="mod-user-submit" id="mod-user-submit" />
+<br /><br />
+<?php process_modify_user(); ?>
+&nbsp;
+</form>
+
+<h3>Moderate Questions</h3>
+<form name="moderate-questions" method="post" action="">
+list questions to modify, selecting one brings up modification dialog
+<br />
+this should include feedback moderation
+<input type="submit" value="Register" name="mod-question-submit" id="mod-question-submit" />
+<br /><br />
+<?php process_moderate_questions(); ?>
+&nbsp;
+</form>
+
 
 <?php
   ########################################## IS A MODERATOR
---
+++ moderator_lib.php
@@ -0,0 +1,27 @@
+<?php
+
+function process_register_user() { 
+  
+  if( isset($_POST["reg-user-submit"]) ) { 
+    echo "stuff";
+  }
+
+}
+
+function process_modify_user() { 
+  
+  if( isset($_POST["mod-user-submit"]) ) { 
+    echo "stuff";
+  }
+
+}
+
+function process_moderate_questions() { 
+  
+  if( isset($_POST["mod-question-submit"]) ) { 
+    echo "stuff";
+  }
+
+}
+
+?>
