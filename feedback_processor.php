a77c1ec3ee2ba425a8af72782f646ad180af6f07
Jean Michel Rouly <jrouly@gmu.edu>
2013-10-25 01:52:17 -0400

    Processes removal requests now, for questions only.

--- feedback_processor.php
+++ feedback_processor.php
@@ -7,47 +7,62 @@
 
   # if the form is properly submitted, with a comment and either a 
   # good or a bad selection, proceed.
-  if(isset($_POST["qid"]) && isset($_POST["r"]) && isset($_POST["f"]) ) { 
-
+  if(isset($_POST["qid"])) { 
+  
     # connect to the SQL database.
     $mysqli = connect_to_mysql();
-
-    # grab POST data
-    $rating_adj = ($_POST["r"] == "b") ? -1 : 1;
-    $comment = $_POST["f"];
     $question_id = $_POST["qid"];
 
-    # clean up the user's comment/feedback
-    $comment = htmlspecialchars( $comment );
-    $comment = $mysqli->real_escape_string( $comment );
-    $comment = addslashes( $comment );
-    $comment = trim( $comment );
+    # If this question is marked for feedback, submit feedback.
+    if( isset($_POST["r"]) && isset($_POST["f"]) &&
+        $_POST["r"] != "" && $_POST["f"] != "" ) { 
 
-    if( # check that the question id is valid
-        $question_id > 0 &&
-        # make sure the comment is valid
-        is_string($comment) && $comment != "" ) { 
+      # grab POST data
+      $rating_adj = ($_POST["r"] == "b") ? -1 : 1;
+      $comment = $_POST["f"];
 
-      $user = get_username_from_hash( $_COOKIE["hash"] );
+      # clean up the user's comment/feedback
+      $comment = htmlspecialchars( $comment );
+      $comment = $mysqli->real_escape_string( $comment );
+      $comment = addslashes( $comment );
+      $comment = trim( $comment );
 
-      global $db_name,$tablec,$tableq;
+      if( # check that the question id is valid
+          $question_id > 0 &&
+          # make sure the comment is valid
+          is_string($comment) && $comment != "" ) { 
 
-      # insert a new comment into the tabelc
-      $sql_insert = "INSERT INTO `$db_name`.`$tablec`" . 
-                    "(`comment_id`, `question_id`, `comment`, `user`) " .
-                    "VALUES(NULL, '$question_id', '$comment', '$user');";
-      if( ! $mysqli->query( $sql_insert ) ) {
-        echo "Erorr unregistered user. Please contact webmaster.";
-        return;
+        $user = get_username_from_hash( $_COOKIE["hash"] );
+
+        global $db_name,$tablec,$tableq;
+
+        # insert a new comment into the tabelc
+        $sql_insert = "INSERT INTO `$db_name`.`$tablec`" . 
+                      "(`comment_id`, `question_id`, `comment`, `user`) " .
+                      "VALUES(NULL, '$question_id', '$comment', '$user');";
+        if( ! $mysqli->query( $sql_insert ) ) {
+          echo "Erorr unregistered user. Please contact webmaster.";
+          return;
+        }
+
+        # update the question rating in tableq
+        $sql_update = "UPDATE `$db_name`.`$tableq` " .
+                      "SET `rating` = rating + $rating_adj " .
+                      "WHERE `question_id` = '$question_id';";
+        if( ! $mysqli->query( $sql_update ) ) { 
+          echo "Error unable to rate question. Please contact webmaster.";
+          return;
+        }
       }
-
-      # update the question rating in tableq
-      $sql_update = "UPDATE `$db_name`.`$tableq` " .
-                    "SET `rating` = rating + $rating_adj " .
-                    "WHERE `question_id` = '$question_id';";
-      if( ! $mysqli->query( $sql_update ) ) { 
-        echo "Error unable to rate question. Please contact webmaster.";
-        return;
+    } # If this question was marked for removal, remove it.
+    else if( isset($_POST["removal"]) ) { 
+      if($_POST["removal"] == "question") { 
+        global $db_name,$tablec,$tableq;
+        $sql_delete = "DELETE FROM `$db_name`.`$tableq` WHERE `question_id`='$question_id';";
+        if( ! $mysqli->query($sql_delete) ) {
+          echo "Error unable to remove question. Please contact webmaster.";
+          return;
+        }
       }
     }
 
