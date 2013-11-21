dbabc053975e32dd06cb2908607f672c07da3cf2
Jean Michel Rouly <michel.rouly@gmail.com>
2013-11-20 22:16:50 -0500

    Restyled page.

--- libout.php
+++ libout.php
@@ -48,62 +48,46 @@ function generate_questions_box() {
     
       #### QUESTION BLOCK ####
       echo "<div id=\"question$qid\" class=\"question-box\">".PHP_EOL;
+
+      #### QUESTION TITLE ####
+      echo "<div class=\"question-box-title\">".PHP_EOL;
+      echo "<div class=\"question-box-title-left\">".PHP_EOL;
+      echo "<a href=\"#\" onClick=\"toggle_display('question-text$qid');return false;\">";
       if( is_moderator() ) { 
+        echo "$name</a>";
+        echo " <a href=\"mailto:$user@gmu.edu\">(email)</a>";
+      } else {
+        echo "FAKENAME</a>";
+      }
+      echo "</div>".PHP_EOL;
 
-        ## Question data
-        #echo "<span class=\"question-id\">[ID: $qid]</span>".PHP_EOL;
-        echo "<span class=\"question-rating\">(RATED: $rating)</span>".PHP_EOL;
-
-        ## Remove Question button
+      if( is_moderator() ) { 
+        echo "<div class=\"question-box-title-right\">".PHP_EOL;
+        echo "(RATED: $rating)".PHP_EOL;
         echo "<button onClick=\"remove_question('$qid');return false;\">";
         echo "Remove Question</button>".PHP_EOL;
         echo "<button onClick=\"change_rating('$qid');return false;\">";
         echo "Change Rating</button>".PHP_EOL;
-
-        echo "<br />".PHP_EOL;
-
+        echo "</div>".PHP_EOL;
       }
 
-      ## Question asker && email
-      echo "<span class=\"question-asker\">Asked by <strong>";
-      if( is_moderator() ) { 
-        echo "<a href=\"mailto:$user@gmu.edu\">$name</a>";
-      } else { 
-        echo "FAKENAME";
-      }
-      echo "</strong>:</span>".PHP_EOL;
-      echo "<br /><br />".PHP_EOL;
-
-      echo "<span class=\"question-text\">$question</span>".PHP_EOL;
-      echo "<br /><br />".PHP_EOL;
-
-      #### LINK BLOCK ####
-      echo "<a href=\"#\" onClick=\"toggle_display('comment-list$qid'); return false;\">Toggle Comments (";
-      echo (($mycomments!=null)?count($mycomments):"0").")</a>".PHP_EOL;
-      echo "<br />".PHP_EOL;
-      echo "<a href=\"#\" onClick=\"toggle_display('make-comment$qid'); return false;\">Provide feedback.</a>";
-      #### LINK BLOCK ####
-
-      #### FEEDBACK BLOCK ####
-      echo "<div id=\"make-comment$qid\" class=\"feedback\" style=\"display:none;\">".PHP_EOL;
-      echo "<textarea class=\"feedback-text\" name=\"c\"></textarea>".PHP_EOL;
-      echo "<br />".PHP_EOL;
-      echo "Your name will be associated with any comments you make.".PHP_EOL;
-      echo "<br />".PHP_EOL;
-      echo "<input type=\"button\" value=\"Good Question\" name=\"g\" ".PHP_EOL;
-      echo "onClick=\"submit_feedback($qid, 'g');\" />".PHP_EOL;
-      echo "<input type=\"button\" value=\"Needs Work\" name=\"b\" ".PHP_EOL;
-      echo "onClick=\"submit_feedback($qid, 'b');\" />".PHP_EOL;
-      echo "<input type=\"button\" value=\"Cancel\" ";
-      echo "onClick=\"hide_display('make-comment$qid');\" />".PHP_EOL;
       echo "</div>".PHP_EOL;
-      #### FEEDBACK BLOCK ####
+      #### QUESTION TITLE ####
+
+      echo "<div class=\"question-text\" id=\"question-text$qid\" style=\"display:none;\">$question</div>".PHP_EOL;
+
+      #### LINK BLOCK ####
+      echo "<div class=\"question-box-footer\">".PHP_EOL;
+      echo "<a href=\"#\" onClick=\"toggle_display('comment-list$qid'); return false;\">Comments (";
+      echo (($mycomments!=null)?count($mycomments):"0").")</a>".PHP_EOL;
+      echo "</div>".PHP_EOL;
+      #### LINK BLOCK ####
 
       #### COMMENT LIST BLOCK ####
       echo "<div id=\"comment-list$qid\" class=\"comment-list-box\" style=\"display:none;\">".PHP_EOL;
       if( $mycomments == null ) { 
         echo "<div class=\"comment-box\">";
-        echo "<span class=\"comment-text\">None yet.</span>".PHP_EOL;
+        echo "<div class=\"comment-text\">None yet.</div>";
         echo "</div>";
       } else { 
         foreach( $mycomments as $cid => $comment ) { 
@@ -116,45 +100,26 @@ function generate_questions_box() {
           #### COMMENT BOX BLOCK ####
           echo "<div id=\"comment$cid\" class=\"comment-box\">".PHP_EOL;
 
+          echo "<span class=\"comment-author\">";
           if( is_moderator() ) { 
-            echo "<span class=\"comment-id\">[ID: $cid]</span>".PHP_EOL;
-            echo "<button onClick=\"remove_comment('$cid');return false;\">";
-            echo "Remove Comment</button>".PHP_EOL;
-            echo "<br />".PHP_EOL;
-          }
-
-          echo "<span class=\"comment-author\"><strong>";
-          if( is_moderator() ) {
-            echo "<a href=\"mailto:$user@gmu.edu\">$name</a>";
+            echo "$name</a>";
+            echo " <a href=\"mailto:$user@gmu.edu\">(email)</a>";
           } else {
-            echo "FAKENAME";
+            echo "FAKENAME</a>";
           }
-          echo "</strong> says:</span>".PHP_EOL;
+          echo " says:</span><br />".PHP_EOL;
 
-          echo "<span class=\"comment-text\">$text</span>".PHP_EOL;
-
-          echo "<br />".PHP_EOL;
+          echo "<div class=\"comment-text\">$text</div>".PHP_EOL;
 
           #### LINK BLOCK ####
-          echo "<a href=\"#\"
-          onClick=\"toggle_display('comment-replies$cid'); return false;\">Toggle Replies (";
+          echo "<a href=\"#\" onClick=\"toggle_display('comment-replies$cid'); return false;\">Replies (";
           echo (($myreplies!=null)?count($myreplies):"0").")</a>".PHP_EOL;
-          echo "<br />".PHP_EOL;
-          echo "<a href=\"#\" onClick=\"toggle_display('make-reply$cid'); return false;\">Reply.</a>";
-          #### LINK BLOCK ####
 
-          #### FEEDBACK BLOCK ####
-          echo "<div id=\"make-reply$cid\" class=\"feedback\" style=\"display:none;\">".PHP_EOL;
-          echo "<textarea class=\"feedback-text\" name=\"reply\"></textarea>".PHP_EOL;
-          echo "<br />".PHP_EOL;
-          echo "Your name will be associated with any comments you make.".PHP_EOL;
-          echo "<br />".PHP_EOL;
-          echo "<input type=\"button\" value=\"Reply\" name=\"reply\" ".PHP_EOL;
-          echo "onClick=\"submit_reply($cid);\" />".PHP_EOL;
-          echo "<input type=\"button\" value=\"Cancel\" ";
-          echo "onClick=\"hide_display('make-reply$cid');\" />".PHP_EOL;
-          echo "</div>".PHP_EOL;
-          #### FEEDBACK BLOCK ####
+          if( is_moderator() ) { 
+            echo " &bull; <button onClick=\"remove_comment('$cid');return false;\">";
+            echo "Remove Comment</button>".PHP_EOL;
+          }
+          #### LINK BLOCK ####
 
           #### REPLIES BOX BLOCK ####
           echo "<div id=\"comment-replies$cid\" class=\"replies-box\" style=\"display:none;\">".PHP_EOL;
@@ -176,19 +141,38 @@ function generate_questions_box() {
                 echo "<button onClick=\"remove_reply('$rid');return false;\">";
                 echo "Remove</button>".PHP_EOL;
               }
-              echo "<span class=\"reply-author\"><strong>".PHP_EOL;
+
+              echo "<span class=\"reply-author\">";
               if( is_moderator() ) {
-                echo "<a href=\"mailto:$user@gmu.edu\">$name</a>".PHP_EOL;
+                echo "$name</a>";
+                echo " <a href=\"mailto:$user@gmu.edu\">(email)</a>";
               } else {
-                echo "FAKENAME";
+                echo "FAKENAME</a>";
               }
-              echo "</strong> says:</span>".PHP_EOL;
-              echo "<span class=\"reply-text\">$text</span>".PHP_EOL;
+              echo " says:</span>".PHP_EOL;
+
+              echo "<div class=\"reply-text\">$text</div>".PHP_EOL;
               echo "</div>".PHP_EOL;
             }
 
           }
 
+          #### FEEDBACK BLOCK ####
+          echo "<div class=\"reply\">";
+          echo "<a href=\"#\" onClick=\"toggle_display('make-reply$cid'); return false;\">Make Reply</a>";
+          echo "</div>".PHP_EOL;
+
+          echo "<div id=\"make-reply$cid\" class=\"reply feedback\" style=\"display:none;\">".PHP_EOL;
+          echo "<textarea class=\"feedback-text\" name=\"reply\"></textarea>".PHP_EOL;
+          echo "<br />".PHP_EOL;
+          echo "Your name will be associated with any comments you make.".PHP_EOL;
+          echo "<br />".PHP_EOL;
+          echo "<input type=\"button\" value=\"Reply\" name=\"reply\" ".PHP_EOL;
+          echo "onClick=\"submit_reply($cid);\" />".PHP_EOL;
+          echo "<input type=\"button\" value=\"Cancel\" ";
+          echo "onClick=\"hide_display('make-reply$cid');\" />".PHP_EOL;
+          echo "</div>".PHP_EOL;
+          #### FEEDBACK BLOCK ####
 
           echo "</div>".PHP_EOL;
           #### REPLIES BOX BLOCK ####
@@ -197,6 +181,26 @@ function generate_questions_box() {
           #### COMMENT BOX BLOCK ####
         }
       }
+
+      #### FEEDBACK BLOCK ####
+      echo "<div id=\"make-comment$qid\" class=\"comment-box feedback\" style=\"display:none;\">".PHP_EOL;
+      echo "<textarea class=\"feedback-text\" name=\"c\"></textarea>".PHP_EOL;
+      echo "<br />".PHP_EOL;
+      echo "Your name will be associated with any comments you make.".PHP_EOL;
+      echo "<br />".PHP_EOL;
+      echo "<input type=\"button\" value=\"Good Question\" name=\"g\" ".PHP_EOL;
+      echo "onClick=\"submit_feedback($qid, 'g');\" />".PHP_EOL;
+      echo "<input type=\"button\" value=\"Needs Work\" name=\"b\" ".PHP_EOL;
+      echo "onClick=\"submit_feedback($qid, 'b');\" />".PHP_EOL;
+      echo "<input type=\"button\" value=\"Cancel\" ";
+      echo "onClick=\"hide_display('make-comment$qid');\" />".PHP_EOL;
+      echo "</div>".PHP_EOL;
+
+      echo "<div class=\"comment-box-footer\">".PHP_EOL;
+      echo "<a href=\"#\" onClick=\"toggle_display('make-comment$qid'); return false;\">Make Comment</a>".PHP_EOL;
+      echo "</div>".PHP_EOL;
+      #### FEEDBACK BLOCK ####
+
       echo "</div>".PHP_EOL;
       #### COMMENT LIST BLOCK ####
 
