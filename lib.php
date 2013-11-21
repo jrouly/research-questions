1858c107ce67b1fdba0db05bc39838b2a6a42ba6
Jean Michel Rouly <michel.rouly@gmail.com>
2013-11-20 23:34:35 -0500

    Removed bad reference to salt.

--- lib.php
+++ lib.php
@@ -6,7 +6,6 @@ require "libusrs.php";
 require "libdb.php";
 require "libout.php";
 
-global $salt;
 global $db_host,$db_user,$db_pass,$db_name;
 global $tableq,$tablec,$tableu,$tablel,$tablea;
 global $ldap_host,$ldap_port;
