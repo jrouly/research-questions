1d77427335f3fc220c552ecfacbc38eb4807db16
Jean Michel Rouly <michel.rouly@gmail.com>
2013-11-22 12:16:53 -0500

    Fixed some repeated paragraphs.

--- instructions.php
+++ instructions.php
@@ -1,16 +1,9 @@
 <?php require "static/top.part"; ?>
 
-<h1>Instructions</h1>
+<h1>Page Contents</h1>
 
-<p>
-  The objective of this exercise is to have you rate your peers' research
-  questions as either <strong>good</strong> or <strong>needs work</strong>.
-  You will also be asked to provide <strong>comments</strong> explaining your
-  thought process. This document is meant to help you understand how best
-  to do this.
-</p>
-
-<ul id="instructions-index">
+<ul class="instructions">
+  <li><a href="#Instructions">Instructions</a></li>
   <li><a href="#Research-Questions">Research Questions</a></li>
   <ul>
     <li><a href="#Good-Questions">What makes a good question?</a></li>
@@ -25,18 +18,29 @@
   </ul>
   <li><a href="#Feedback">Feedback</a></li>
   <ul>
-    <li><a href="#Good-Feedback">Good Feedback</a></li>
-    <li><a href="#Bad-Feedback">Bad Feedback</a></li>
+    <li><a href="#Constructive-Feedback">Constructive Feedback</a></li>
+    <li><a href="#Non-Constructive-Feedback">Non-Constructive Feedback</a></li>
     <li><a href="#Feedback-Example">Feedback Example</a></li>
     <li><a href="#Pro-Tip">Pro Tip</a></li>
   </ul>
 </ul>
 
-<h1>Research Questions</h1>
-<a name="Research-Questions"></a>
+<a name="Instructions"></a>
+<h1>Instructions</h1>
 
-<h2>Good Questions</h2>
+<p>
+  The objective of this exercise is to have you rate your peers' research
+  questions as either <strong>good</strong> or <strong>needs work</strong>.
+  You will also be asked to provide <strong>comments</strong> explaining your
+  thought process. This document is meant to help you understand how best
+  to do this.
+</p>
+
+<a name="Research-Questions"></a>
+<h1>Research Questions</h1>
+
 <a name="Good-Questions"></a>
+<h2>Good Questions</h2>
 
 <p> The art of constructing a good research question is a difficult skill
 to learn, but with persistence and dedication can be mastered in a short
@@ -49,28 +53,28 @@ receive.</p>
 your research question. When beginning to write the question, keep in mind
 the following factors: </p>
 
-<h3>Uniqueness</h3>
 <a name="Uniqueness"></a>
+<h3>Uniqueness</h3>
 
 <p>Your research question should be unique - this means that either no one
 has previously asked this question or that your work presents a new line of
 thought on an existing controversy.</p>
 
-<h3>Framing</h3>
 <a name="Framing"></a>
+<h3>Framing</h3>
 
 <p>The best research questions look at the "hows" and "whys" rather than
 the "whats" and "whos." On occasion, a good research question can begin
 with "what" (or even more rarely with "who") if it meets all the other
 necessary criteria.</p>
 
-<p>The best research questions look at the "hows" and "whys" rather than
-the "whats" and "whos." On occasion, a good research question can begin
-with "what" (or even more rarely with "who") if it meets all the other
-necessary criteria.</p>
+<p>You do not want to have a question that can definitively be answered.
+The answer to your research question should be debatable. A question asking
+"who" or "what" can almost always be answered conclusively, and therefore
+is not a proper research question.</p>
 
-<h3>Focus</h3>
 <a name="Focus"></a>
+<h3>Focus</h3>
 
 <p>Your research question should be asking a specific question that is
 narrow. For example, <em>"How did President Clinton's Foreign Policy
@@ -85,8 +89,8 @@ Clinton's foreign policy towards Southern France and Northern Italy between
 1995 and 1999 influence the economy in America’s Northeast?"</em></p>
 
 
-<h3>Completeness</h3>
 <a name="Completeness"></a>
+<h3>Completeness</h3>
 
 <p>Remember that your research question should include the following four
 components – who, what, where and when. In our example of a good research
@@ -101,15 +105,15 @@ you would need to clarify what you consider to be "<em>late
 1990s."</em></p>
 
 
-<h3>Researchability</h3>
 <a name="Researchability"></a>
+<h3>Researchability</h3>
 
 <p>Make sure your research question has enough available scholarly sources
 for you to write a research paper (although this should have been confirmed
 when choosing your research topic).</p>
 
-<h2>Example Questions</h2>
 <a name="Example-Questions"></a>
+<h2>Example Questions</h2>
 
 <h3>Needs Improvement: <em>"Who killed President John F. Kennedy?"</em></h3>
 
@@ -125,7 +129,8 @@ debate the ‘lone gunman theory’ of the JFK Assassination?"</em></p>
 Commission establish a foundation for theories of conspiracy concerning the
 JFK Assassination?"</em>
 
-<h3>Needs Improvement: <em>"Will Hillary Clinton run for President in 2016?"</em></h3>
+<h3>Needs Improvement: <em>"Will Hillary Clinton run for President in
+2016?"</em></h3>
 
 <p>This question is far too broad - there are many possibilities that can
 be explored - and the scholarship on this subject is likely limited, as it
@@ -144,8 +149,8 @@ success of future candidacies?"</em></p>
 Presidency of Barack Obama?"</em></p>
 
 
-<h1>Feedback</h1>
 <a name="Feedback"></a>
+<h1>Feedback</h1>
 
 <p>So, you and your classmates have completed the colossal task of creating
 your research questions. Unfortunately, a question that may make sense to
@@ -156,32 +161,14 @@ criticism of others and to adjust your question accordingly each time.
 Allow suggestions from others as well as the information you gain from
 scholarly articles guide your research process.</p>
 
-<h2>Good Feedback</h2>
-<a name="Good-Feedback"></a>
+<a name="Constructive-Feedback"></a>
+<h2>Constructive Feedback</h2>
 
-<p>First of all, it is very important to note that <strong>positive
-feedback isn't always good and negative feedback isn't always bad</strong>.
-Remember that the purpose of providing feedback isn't to make your
-classmate feel better about their research projects; it is to help them do
-better with their research projects.</p>
-
-<p>Good feedback encourages the researcher, but also pushes them to improve
-their question by letting him/her know what may be unclear to others.</p>
-
-<p>Look at the question critically and identify where improvements could be
-made in order to make the question more specific and answerable. Offer
-these suggestions in your feedback. It is always better to offer an
-alternative phrase or word in order to make it clear to the researcher
-exactly how they can improve the question.</p>
-
-<h2>Bad Feedback</h2>
-<a name="Bad-Feedback"></a>
-
-<p>First of all, it is very important to note that positive feedback isn't
-always good and negative feedback isn't always bad. Remember that the
-purpose of providing feedback isn't to make your classmate feel better
-about their research projects; it is to help them do better with their
-research projects.</p>
+<p>First of all, it is very important to note that <strong><em>positive
+feedback isn't always good and negative feedback isn't always
+bad</em></strong>.  Remember that the purpose of providing feedback isn't
+to make your classmate feel better about their research projects; it is to
+help them do better with their research projects.</p>
 
 <p>Good feedback encourages the researcher, but also pushes them to improve
 their question by letting him/her know what may be unclear to others.</p>
@@ -192,9 +179,20 @@ these suggestions in your feedback. It is always better to offer an
 alternative phrase or word in order to make it clear to the researcher
 exactly how they can improve the question.</p>
 
+<a name="Non-Constructive-Feedback"></a>
+<h2>Non-Constructive Feedback</h2>
 
-<h2>Feedback Example</h2>
+<p>Just like a bad research question, bad feedback is vague and unhelpful.
+Feedback is least useful when it is dishonest, biased, too critical, or
+unspecific. Be as honest as possible to the researcher to allow them to
+make changes wherever possible. Never hold back constructive criticism for
+fear of hurting your classmate's feelings. Instead, do them a favor and
+help them create the best question they can! Always remember to tell them
+exactly where and how they can make improvements to their question, but
+always remember to praise them for what they did right.</p>
+
 <a name="Feedback-Example"></a>
+<h2>Feedback Example</h2>
 
 <p>If your classmate's question is, <em>"Who killed President John F.
 Kennedy?"</em></p>
@@ -206,13 +204,14 @@ good research question should be, it is slightly unfocused. Propose a
 certain detail surrounding JFK's assassination that they should focus on in
 order to streamline their research and produce a good paper.</p>
 
-<p><strong>Do not</strong> tell the researcher that this is a good question if you are aware
-that it is not. This type of positive feedback is dishonest, and dishonest
-feedback is always bad feedback. Your main aim should be to help your peers
-improve the way that you’d like them to help YOU improve.</p>
+<p><strong><em>Do not</em></strong> tell the researcher that this is a good question
+if you are aware that it is not. This type of positive feedback is
+dishonest, and dishonest feedback is always bad feedback. Your main aim
+should be to help your peers improve the way that you’d like them to help
+YOU improve.</p>
 
-<h2>Pro Tip</h2>
 <a name="Pro-Tip"></a>
+<h2>Pro Tip</h2>
 
 <p>Listen to the feedback you receive. If your question is unclear to one
 of your peers, chances are it may also be unclear to others. Be mindful of
@@ -221,5 +220,7 @@ you succeed. Never be afraid of asking for feedback, and do not become
 discouraged if the feedback you receive is not what you would have hoped.
 You are all in the same Honors 110 boat, and by giving and receiving honest
 feedback, you are setting yourselves up for nothing short of success.</p>
+
+<p class="instructions"><a href="#top">Return to top.</a></p>
 
 <?php require "static/bottom.part"; ?>
