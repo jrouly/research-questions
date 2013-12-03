aa7f78468bab289a27db90f06570d8e479be1ba8
Jean Michel Rouly <michel.rouly@gmail.com>
2013-12-03 16:40:53 -0500

    Added custom textarea css.

--- researchquestions/website/forms.py
+++ researchquestions/website/forms.py
@@ -1,7 +1,7 @@
 from website.models import Question, Comment, Reply
 
 from django.db import models
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea
 
 class QuestionForm( ModelForm ):
     class Meta:
@@ -9,6 +9,9 @@ class QuestionForm( ModelForm ):
         fields = ('text',)
         exclude = ('user','date','rating')
         localized_fields = ('date',)
+        widgets = {
+            'text':Textarea(attrs={'class': 'form-control',}),
+        }
 
 class CommentForm( ModelForm ):
     class Meta:
