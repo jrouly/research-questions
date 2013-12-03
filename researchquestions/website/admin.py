344eda441506c07dcb3d2534de52a706fdc88a50
Jean Michel Rouly <michel.rouly@gmail.com>
2013-12-03 00:22:51 -0500

    Don't register the custom User class anymore.

--- researchquestions/website/admin.py
+++ researchquestions/website/admin.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.contrib import admin
-from website.models import Question, Comment, Reply, User
+from website.models import Question, Comment, Reply
 
 # Register your models here.
 admin.site.register(Question)
 admin.site.register(Comment)
 admin.site.register(Reply)
-admin.site.register(User)
