fa0bd9d6a298a0aa622a2b3caed4ee0fd375f2b4
Jean Michel Rouly <michel.rouly@gmail.com>
2013-12-03 00:46:26 -0500

    Login-protected question submission and index pages.

--- researchquestions/website/views.py
+++ researchquestions/website/views.py
@@ -2,6 +2,7 @@ from website.models import Question, Comment, Reply, User
 from django.conf import settings
 from django.shortcuts import render_to_response, get_object_or_404
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 import requests
 
@@ -11,7 +12,14 @@ def instructions(request):
     },
     )
 
+@login_required
 def submit_question(request):
     return render_to_response('submit_question.html', {
+    },
+    )
+
+@login_required
+def index(request):
+    return render_to_response('index.html', {
     },
     )
