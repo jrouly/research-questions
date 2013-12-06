beb29c3ef71ece2855266eda2f2ba5ea090b46d1
Jean Michel Rouly <michel.rouly@gmail.com>
2013-12-06 07:09:20 -0500

    Removed setting thing.

--- researchquestions/settings/settings.py
+++ researchquestions/settings/settings.py
@@ -65,7 +65,6 @@ MEDIAFILES_DIRS = (
 STATIC_ROOT = (os.path.join(BASE_DIR, 'static/'))
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
 )
 
 STATICFILES_FINDERS = (
