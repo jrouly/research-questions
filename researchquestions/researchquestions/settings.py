c887983de506cbcf2b4b782573efd3dc71f74330
Jean Michel Rouly <michel.rouly@gmail.com>
2014-02-28 12:47:26 -0500

    Installed custom template context processor.

--- researchquestions/researchquestions/settings.py
+++ researchquestions/researchquestions/settings.py
@@ -84,9 +84,17 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.app_directories.Loader',
 )
 
-TEMPLATE_CONTEXT_PROCSSORS = (
-    'django.core.context_processors.request',
+TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+
+    'researchquestions.context_processors.branding',
 )
 
 # Database
