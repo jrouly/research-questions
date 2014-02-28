eb7bebf36b96d3681606086b77dc7b3ae606c73d
Jean Michel Rouly <michel.rouly@gmail.com>
2014-02-28 13:54:20 -0500

    Removed helppages app, moved university to just a config directory.

---
+++ researchquestions/config/config.py
@@ -0,0 +1,9 @@
+## Define your install-specific configurations here.
+
+DICTIONARY_ADJECTIVES = 'dicts/adjectives.en'
+DICTIONARY_NOUNS = 'dicts/nouns.en'
+
+PAGE_TITLE_PREFIX = "HNRS 110"
+
+ORGANIZATION = 'George Mason University'
+ORGANIZATION_URL = 'http://www.gmu.edu'
