3e2c3e4fd897303555ff3ecc7729eb9517dc56c6
Jean Michel Rouly <michel.rouly@gmail.com>
2014-02-28 15:36:03 -0500

    Removed config from version control again

--- researchquestions/config/config.py
+++
@@ -1,9 +0,0 @@
-## Define your install-specific configurations here.
-
-DICTIONARY_ADJECTIVES = 'dicts/adjectives.en'
-DICTIONARY_NOUNS = 'dicts/nouns.en'
-
-PAGE_TITLE_PREFIX = "HNRS 110"
-
-ORGANIZATION = 'George Mason University'
-ORGANIZATION_URL = 'http://www.gmu.edu'
