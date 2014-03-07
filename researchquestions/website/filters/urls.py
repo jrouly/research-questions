0e8d76fe7001a593bd94d60ed931682b4f3e7fda
Jean Michel Rouly <michel.rouly@gmail.com>
2014-03-06 21:09:13 -0500

    Redirect invalid filter urls to base_redirect

--- researchquestions/website/filters/urls.py
+++ researchquestions/website/filters/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('website.filters.views',
 
     #### Available filters
-    url(r'^class/(?P<class>\w+)$', 'filter_class', name='filter_class'),
+    url(r'^$', 'base_redirect'),
+    url(r'^section/(?P<section>\w+)$', 'filter_section', name='filter_section'),
+    url(r'^section/?$', 'base_redirect'),
     url(r'^date$', 'filter_date', name='filter_date'),
     url(r'^comments$', 'filter_comments', name='filter_comments'),
 
