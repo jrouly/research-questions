0fe6a770175f6f2756867bba3a82fb6a7a696b42
Jean Michel Rouly <michel.rouly@gmail.com>
2014-03-08 01:56:06 -0500

    Only accept correct section filters.

--- researchquestions/website/filters/urls.py
+++ researchquestions/website/filters/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('website.views',
 
     #### Available filters
-    url(r'^section/(?P<section>\w+)$', 'index'),
+    url(r'^section/(?P<section>[a-zA-Z]+ {0,1}[0-9]*)$', 'index'),
     url(r'^date$', 'index', {'sort': 'date'}),
     url(r'^comments$', 'index', {'sort': 'comments'}),
 
