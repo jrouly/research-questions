953a35e974c3e920b7ee68272d5e3adece5d0368
Jean Michel Rouly <michel.rouly@gmail.com>
2014-03-07 23:56:33 -0500

    Pass kwargs from filter url to index view.

--- researchquestions/website/filters/urls.py
+++ researchquestions/website/filters/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 
-urlpatterns = patterns('website.filters.views',
+urlpatterns = patterns('website.views',
 
     #### Available filters
-    url(r'^section/(?P<section>\w+)$', 'filter_section', name='filter_section'),
-    url(r'^date$', 'filter_date', name='filter_date'),
-    url(r'^comments$', 'filter_comments', name='filter_comments'),
+    url(r'^section/(?P<section>\w+)$', 'index'),
+    url(r'^date$', 'index', {'sort': 'date'}),
+    url(r'^comments$', 'index', {'sort': 'comments'}),
 
     #### Invalid requests to redirect
 #    url(r'^$', 'base_redirect'),
