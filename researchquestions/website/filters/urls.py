9a068e3b9d2ee71fc4a1d5f7e2c416790d977a04
Jean Michel Rouly <michel.rouly@gmail.com>
2014-03-08 02:31:21 -0500

    Named filter urls

--- researchquestions/website/filters/urls.py
+++ researchquestions/website/filters/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('website.views',
 
     #### Available filters
-    url(r'^section/(?P<section>[a-zA-Z]+ {0,1}[0-9]*)$', 'index'),
-    url(r'^date$', 'index', {'sort': 'date'}),
-    url(r'^comments$', 'index', {'sort': 'comments'}),
+    url(r'^section/(?P<section>[a-zA-Z]+ {0,1}[0-9]*)$', 'index', name='filter_by_section'),
+    url(r'^date$', 'index', {'sort': 'date'}, name='sort_by_date'),
+    url(r'^comments$', 'index', {'sort': 'comments'}, name='sort_by_comments'),
 
 )
