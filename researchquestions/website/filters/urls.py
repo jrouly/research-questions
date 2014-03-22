720984e5909f654d1c6b0f24189ce82b1589e60b
Jean Michel Rouly <michel.rouly@gmail.com>
2014-03-21 21:00:58 -0400

    Generalized sort urls.

--- researchquestions/website/filters/urls.py
+++ researchquestions/website/filters/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('website.views',
 
     #### Available filters
-    url(r'^section/(?P<section>[a-zA-Z]+ {0,1}[0-9]*)$', 'index', name='filter_by_section'),
-    url(r'^date$', 'index', {'sort': 'date'}, name='sort_by_date'),
-    url(r'^comments$', 'index', {'sort': 'comments'}, name='sort_by_comments'),
+    url(r'^section/(?P<section>[a-zA-Z]+ {0,1}[0-9]*)$', 'index',
+        name='filter'),
+    url(r'^(?P<sort>[a-z]*)$', 'index', name='sort'),
+
+    url(r'^section/(?P<section>[a-zA-Z]+ {0,1}[0-9]*)/(?P<sort>[a-z]*)$',
+        'index', name='filter'),
 
 )
