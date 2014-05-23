971b52d846ff9a48766bc75df628860f31eb0e24
Jean Michel Rouly <michel.rouly@gmail.com>
2014-05-23 11:05:36 -0400

    Correctly create new users from CAS, with a slight email hack.

--- researchquestions/website/cas_callbacks.py
+++ researchquestions/website/cas_callbacks.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import User
+from django.conf import settings
 
 def create_user(tree):
+
     username = tree[0][0].text
-
     user, user_created = User.objects.get_or_create(username=username)
-    profile, created = user.get_profile()
 
-    profile.email = tree[0][1].text
-    profile.position = tree[0][2].text
-    profile.save()
+    if user_created:
+        user.email = "%s@%s" % (username, settings.ORGANIZATION_EMAIL_DOMAIN)
+        user.save()
+
+        print("Created user %s!" % username)
