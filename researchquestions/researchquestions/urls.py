4d8033e1026242ffd2e0aa8c4bdec20bc9a5be33
Jean Michel Rouly <michel.rouly@gmail.com>
2014-08-14 16:39:38 -0400

    Eew

--- researchquestions/researchquestions/urls.py
+++ researchquestions/researchquestions/urls.py
@@ -8,21 +8,20 @@ admin.autodiscover()
 handler404 = 'website.views.error_404'
 handler500 = 'website.views.error_500'
 
-prefix = settings.URL_PREFIX.lstrip('/')
-print( prefix )
+prefix = settings.URL_PREFIX.lstrip("/")
 
 urlpatterns = patterns('',
 
     #### Dynamic website pages
-    url(r'^' + prefix + '/', include('website.urls')),
-    url(r'^' + prefix + '/filter/', include('website.filters.urls')),
+    url(r'^' + prefix + '', include('website.urls')),
+    url(r'^' + prefix + 'filter/', include('website.filters.urls')),
 
     #### Admin pages
-    url(r'^' + prefix + '/admin/', include(admin.site.urls)),
+    url(r'^' + prefix + 'admin/', include(admin.site.urls)),
 )
 
 urlpatterns += patterns('django.contrib.auth.views',
     #### AUTH PAGES ####
-    url(r'^' + prefix + '/login$', 'login', name='website_login'),
-    url(r'^' + prefix + '/logout$', 'logout', {'next_page': '/'}, name='website_logout'),
+    url(r'^' + prefix + 'login$', 'login', name='website_login'),
+    url(r'^' + prefix + 'logout$', 'logout', {'next_page': '/'}, name='website_logout'),
 )
